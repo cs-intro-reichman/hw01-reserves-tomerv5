600014c72b8864b5d38a035e69b81450657b6881
tomerv5 <tomer.vaener@post.runi.ac.il>
2024-01-19 17:04:23 +0200

    Add awesome code for Homework 1

--- GenThree.java
+++ GenThree.java
@@ -1,4 +1,4 @@
-public class Gen3{
+public class GenThree{
     public static void main(String[] args) {
         int min = Integer.parseInt(args[0]);
         int max = Integer.parseInt(args[1]);
--- Triangle.java
+++ Triangle.java
@@ -1,6 +1,5 @@
 public class Triangle {
     public static void main(String[] args) {
-    System.out.println("Enter Triangle lengths: ");
     int a = Integer.parseInt(args[0]);
     int b = Integer.parseInt(args[1]);
     int c = Integer.parseInt(args[2]);
