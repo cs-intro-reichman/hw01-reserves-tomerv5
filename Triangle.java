bbb9b6fff617edab21fccad6ee10a0f5b3befe87
tomerv5 <tomer.vaener@post.runi.ac.il>
2024-01-20 23:15:14 +0200

    Add awesome code for Homework 1

--- Triangle.java
+++ Triangle.java
@@ -1,13 +1,10 @@
 public class Triangle {
     public static void main(String[] args) {
+    System.out.println("Enter Triangle lengths: ");
     int a = Integer.parseInt(args[0]);
     int b = Integer.parseInt(args[1]);
     int c = Integer.parseInt(args[2]);
-        if(a + b > c && b + c > a && c + a > b){
-            System.out.println(a + ", " + b + ", " + c + ": " + "true");
-        }
-        else {
-            System.out.println(a + ", " + b + ", " + c + ": " + "false");
-        }
-    }
+    Boolean isTriangle = a + b > c && b + c > a && c + a > b;
+    System.out.println(a + ", " + b + ", " + c + ": " + isTriangle);
+    }   
 }
